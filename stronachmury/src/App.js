9caf2d924a3fee3d95920e8a3e7bb2539934e559
KamilNowakk <Kamil.nowak9@interia.pl>
2025-05-07 22:10:36 +0200

    Update App.js

--- stronachmury/src/App.js
+++ stronachmury/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Ładowanie...");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch(() => setMessage("Błąd podczas ładowania."));
+  }, []);
+
   return (
-    
+    <div className="App">
+      <h1>Witaj na stronie React + Azure!</h1>
+      <p>{message}</p>
+    </div>
   );
 }
 
